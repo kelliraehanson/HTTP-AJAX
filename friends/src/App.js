8f96ce9e0bceff7bb294b285066448c6390fe278
Kelli Rae Hanson <kelliraehanson@gmail.com>
2019-01-23 14:46:14 -0600

    making files and starting to edit code

--- friends/src/App.js
+++ friends/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +17,9 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React!!!
           </a>
-        </header>
+        </header> */}
       </div>
     );
   }
